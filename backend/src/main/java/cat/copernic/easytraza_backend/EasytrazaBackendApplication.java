e90cb30204ff4b281d5236ad0204ed87c518675c
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-04-13 12:19:42 +0200

    Inicialitzar el projecte 4 Easy Traza

---
+++ backend/src/main/java/cat/copernic/easytraza_backend/EasytrazaBackendApplication.java
@@ -0,0 +1,13 @@
+package cat.copernic.easytraza_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasytrazaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasytrazaBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cat/copernic/easytraza_backend/EasytrazaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.copernic.easytraza_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasytrazaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
