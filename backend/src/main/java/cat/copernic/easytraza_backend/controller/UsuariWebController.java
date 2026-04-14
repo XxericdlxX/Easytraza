da8882430838929063c7ddbf70f9609cecac70e8
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-04-14 17:21:34 +0200

    Afegir rutes web per crear i guardar usuaris

--- backend/src/main/java/cat/copernic/easytraza_backend/controller/UsuariWebController.java
+++ backend/src/main/java/cat/copernic/easytraza_backend/controller/UsuariWebController.java
@@ -1,11 +1,11 @@
 package cat.copernic.easytraza_backend.controller;
 
+import cat.copernic.easytraza_backend.model.Usuari;
 import cat.copernic.easytraza_backend.service.UsuariService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/web/usuaris")
@@ -18,5 +18,17 @@ public class UsuariWebController {
     public String llistarUsuaris(Model model) {
         model.addAttribute("usuaris", usuariService.findAll());
         return "usuaris/Llistar-usuaris";
+    }
+
+    @GetMapping("/crear")
+    public String mostrarFormulariCrearUsuari(Model model) {
+        model.addAttribute("usuari", new Usuari());
+        return "usuaris/crear-usuaris";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarUsuari(@ModelAttribute Usuari usuari) {
+        usuariService.save(usuari);
+        return "redirect:/web/usuaris";
     }
 }
