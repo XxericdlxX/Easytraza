796a95b6cc513c4ab83a8ec6407b7ebbade721a3
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-04-15 21:09:58 +0200

    Configurar retrofit al projecte mobile

--- mobile/app/src/main/java/cat/copernic/easytraza_mobile/network/BackendApiService.kt
+++ mobile/app/src/main/java/cat/copernic/easytraza_mobile/network/BackendApiService.kt
@@ -1,2 +1,10 @@
 package cat.copernic.easytraza_mobile.network
 
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface BackendApiService {
+
+    @GET("/")
+    suspend fun checkConnection(): Response<String>
+}
--- mobile/app/src/main/java/cat/copernic/easytraza_mobile/network/RetrofitClient.kt
+++ mobile/app/src/main/java/cat/copernic/easytraza_mobile/network/RetrofitClient.kt
@@ -1,2 +1,15 @@
 package cat.copernic.easytraza_mobile.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+
+    fun create(baseUrl: String): BackendApiService {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(BackendApiService::class.java)
+    }
+}
