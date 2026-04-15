b572472173b16fbd660cf50f54c191e7f3174a23
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-04-15 21:00:38 +0200

    Adaptar projecte mobile a built-in kotlin dagp 9 i corregir configuració de gradle + Afegir dependencies de retrofit viewmodel i datastore al projecte mobile

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "cat.copernic.easytraza_mobile"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "cat.copernic.easytraza_mobile"
@@ -30,10 +26,12 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     buildFeatures {
         compose = true
     }
@@ -42,12 +40,20 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.kotlinx.coroutines.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
