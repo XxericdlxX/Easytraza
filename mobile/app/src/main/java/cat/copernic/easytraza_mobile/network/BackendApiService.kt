15fcabb9f6365306b709cbac9f0ca6206e431d36
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-04-15 21:43:42 +0200

    Adaptar servei retrofit mobile per consumir l'endpoint json de prova

--- mobile/app/src/main/java/cat/copernic/easytraza_mobile/network/BackendApiService.kt
+++ mobile/app/src/main/java/cat/copernic/easytraza_mobile/network/BackendApiService.kt
@@ -3,8 +3,12 @@ package cat.copernic.easytraza_mobile.network
 import retrofit2.Response
 import retrofit2.http.GET
 
+data class ConnectionResponse(
+    val message: String
+)
+
 interface BackendApiService {
 
-    @GET("/")
-    suspend fun checkConnection(): Response<String>
+    @GET("/api/test-connection")
+    suspend fun checkConnection(): Response<ConnectionResponse>
 }
