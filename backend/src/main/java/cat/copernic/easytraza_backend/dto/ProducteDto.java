5a799945978c7838e5384b4bf3a9bf8ecba70a66
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-04-20 13:35:36 +0200

    Afegir el camp nom i la validació del DTO Producte #85

--- backend/src/main/java/cat/copernic/easytraza_backend/dto/ProducteDto.java
+++ backend/src/main/java/cat/copernic/easytraza_backend/dto/ProducteDto.java
@@ -7,6 +7,10 @@ public class ProducteDto {
 
     private Long id;
 
+    @NotBlank(message = "{productes.nom.obligatori}")
+    @Size(max = 100, message = "{productes.nom.max}")
+    private String nom;
+
     @NotBlank(message = "{productes.descripcio.obligatoria}")
     @Size(max = 255, message = "{productes.descripcio.max}")
     private String descripcio;
@@ -14,8 +18,9 @@ public class ProducteDto {
     public ProducteDto() {
     }
 
-    public ProducteDto(Long id, String descripcio) {
+    public ProducteDto(Long id, String nom, String descripcio) {
         this.id = id;
+        this.nom = nom;
         this.descripcio = descripcio;
     }
 
@@ -25,6 +30,14 @@ public class ProducteDto {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 
     public String getDescripcio() {
