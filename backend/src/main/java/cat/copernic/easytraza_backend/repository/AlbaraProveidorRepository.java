aa78fe5a3dc543b26af682c8f6fb9495eb00daa0
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-04-21 17:42:10 +0200

    Carregar proveïdor, usuari receptor i lots al repository d'albarans de proveïdor per evitar errors al llistat #89

--- backend/src/main/java/cat/copernic/easytraza_backend/repository/AlbaraProveidorRepository.java
+++ backend/src/main/java/cat/copernic/easytraza_backend/repository/AlbaraProveidorRepository.java
@@ -1,18 +1,31 @@
 package cat.copernic.easytraza_backend.repository;
 
 import cat.copernic.easytraza_backend.model.AlbaraProveidor;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AlbaraProveidorRepository extends JpaRepository<AlbaraProveidor, Long> {
 
+    @Override
+    @EntityGraph(attributePaths = {"proveidor", "usuariReceptor", "lots"})
+    List<AlbaraProveidor> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {"proveidor", "usuariReceptor", "lots"})
+    Optional<AlbaraProveidor> findById(Long id);
+
+    @EntityGraph(attributePaths = {"proveidor", "usuariReceptor", "lots"})
     List<AlbaraProveidor> findByProveidor_CifContainingIgnoreCase(String cif);
 
+    @EntityGraph(attributePaths = {"proveidor", "usuariReceptor", "lots"})
     List<AlbaraProveidor> findByDataRecepcio(LocalDate dataRecepcio);
 
+    @EntityGraph(attributePaths = {"proveidor", "usuariReceptor", "lots"})
     List<AlbaraProveidor> findByProveidor_CifContainingIgnoreCaseAndDataRecepcio(String cif, LocalDate dataRecepcio);
 }
