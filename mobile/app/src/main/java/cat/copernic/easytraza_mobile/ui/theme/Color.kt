89b0481eaac4329ebe3758ef30c9152bef768e5b
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-04-22 10:10:00 +0200

    Adaptar la paleta de colors del mòdul mobile a l'estil visual d'EasyTraza #97

--- mobile/app/src/main/java/cat/copernic/easytraza_mobile/ui/theme/Color.kt
+++ mobile/app/src/main/java/cat/copernic/easytraza_mobile/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package cat.copernic.easytraza_mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val EasyTrazaBackground = Color(0xFFF8F4EE)
+val EasyTrazaSurface = Color(0xFFFFFDF9)
+val EasyTrazaPrimary = Color(0xFF8B5E3C)
+val EasyTrazaOnPrimary = Color(0xFFFFFFFF)
+val EasyTrazaPrimaryContainer = Color(0xFFF6EEE7)
+val EasyTrazaOnPrimaryContainer = Color(0xFF4A2E1F)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val EasyTrazaSecondary = Color(0xFFD9A441)
+val EasyTrazaOnSecondary = Color(0xFF4A2E1F)
+val EasyTrazaSecondaryContainer = Color(0xFFFBF3DD)
+val EasyTrazaOnSecondaryContainer = Color(0xFF4A2E1F)
+
+val EasyTrazaTertiary = Color(0xFF8FAF7A)
+val EasyTrazaOnTertiary = Color(0xFF4A2E1F)
+val EasyTrazaError = Color(0xFFB04A3A)
+val EasyTrazaOnError = Color(0xFFFFFFFF)
+
+val EasyTrazaOutline = Color(0xFFD8C8B8)
+val EasyTrazaOnBackground = Color(0xFF4A2E1F)
+val EasyTrazaOnSurface = Color(0xFF4A2E1F)
+val EasyTrazaOnSurfaceVariant = Color(0xFF7A5A46)
