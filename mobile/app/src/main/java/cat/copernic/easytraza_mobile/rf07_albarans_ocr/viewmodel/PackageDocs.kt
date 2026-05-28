12e217de7811fb03e0bdc9a0d9fc4e7331e767c6
Eric Delgado López <delgado.lopez.eric@alumnat.copernic.cat>
2026-05-28 19:01:16 +0200

    #44 Ajusta el KDoc dels ViewModels mobile

--- mobile/app/src/main/java/cat/copernic/easytraza_mobile/rf07_albarans_ocr/viewmodel/PackageDocs.kt
+++ mobile/app/src/main/java/cat/copernic/easytraza_mobile/rf07_albarans_ocr/viewmodel/PackageDocs.kt
@@ -1,7 +1,14 @@
 /**
- * Interfície d'usuari d'autenticació mobile.
+ * ViewModels del RF07 - Recepció d'albarans de proveïdor amb OCR.
  *
- * Aquest package conté les pantalles i components visuals relacionats amb la
- * selecció, identificació i gestió de sessió de l'usuari.
+ * Aquest package conté la lògica de presentació relacionada amb la recepció
+ * assistida d'albarans des de l'aplicació mobile.
+ *
+ * Els ViewModels gestionen l'estat de la pantalla, coordinen les accions de
+ * selecció o captura del document, envien les dades al backend i preparen el
+ * resultat OCR perquè l'usuari el pugui revisar abans de confirmar l'albarà.
+ *
+ * Aquesta capa separa la lògica de pantalla de les crides de xarxa i dels
+ * casos d'ús del RF07.
  */
-package cat.copernic.easytraza_mobile.rf25_rf28_rf29_auth.ui
+package cat.copernic.easytraza_mobile.rf07_albarans_ocr.viewmodel
--- mobile/app/src/main/java/cat/copernic/easytraza_mobile/rf10_rf11_rf21_rf22_lots/viewmodel/PackageDocs.kt
+++ mobile/app/src/main/java/cat/copernic/easytraza_mobile/rf10_rf11_rf21_rf22_lots/viewmodel/PackageDocs.kt
@@ -2,7 +2,7 @@
  * ViewModels de la gestió mobile de lots.
  *
  * Aquest package conté la lògica de presentació de les pantalles de lots,
- * incloent estat de càrrega, filtres, ordenació i accions d'iniciar o
+ * incloent-hi estat de càrrega, filtres, ordenació i accions d'iniciar o
  * finalitzar lots.
  */
 package cat.copernic.easytraza_mobile.rf10_rf11_rf21_rf22_lots.viewmodel
